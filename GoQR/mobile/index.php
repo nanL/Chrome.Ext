2a77b209debf177233ccfa22b4d0e03ae13a7d50
nan <x@nan.im>
2013-04-26 15:52:51 +0800

    upload GoQR old ver.

---
+++ GoQR/mobile/index.php
@@ -0,0 +1,13 @@
+<title>GoQR for Mobile</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no;">
+
+<dl>
+<dt><a target="_blank" href="../">· Web APP 模式</a>：</dt><dd>该模式支持多数移动设备，直接使用手机浏览器打开即可；</dd>
+<dt><a target="_blank" href="GoQR-iOS.mobileconfig">· iOS 私有模式</a>：</dt><dd>该模式仅支持 iOS 设备即苹果 iPhone iPodTouch 等等，提供一个配置文件支持在屏幕上添加一个启动 GoQR 的快捷方式图标；</dd>
+</dl>
+
+<style>
+dd { margin-bottom:20px; }
+</style>
